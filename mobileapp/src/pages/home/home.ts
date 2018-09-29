24b4e8b53dd8e6d1a07c072521b73e36e1f5f2d5
Murtaz <36333209+itsmurtazmastim@users.noreply.github.com>
2018-09-29 14:01:58 +0530

    Update home.ts

--- mobileapp/src/pages/home/home.ts
+++ mobileapp/src/pages/home/home.ts
@@ -11,6 +11,10 @@ let url:any='http://localhost:3000'
   constructor(public navCtrl: NavController,private http:Http) {
 
   }
+
+switchChange(state){
+  console.log('state :: ',state)
+this.callGetApi()}
   callGetApi(uri){
     
 this.http.get(this.url+uri)
